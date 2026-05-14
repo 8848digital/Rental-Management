eae6a9f0c724d4fdf86f8662d0ff4adb56be62f6
Mahak Mujawadiya08 <mahak@8848digital.com>
2026-05-14 09:28:33 +0000

    feat: loa doc changes

--- rental_management/rental_management/doctype/loa/loa.py
+++ rental_management/rental_management/doctype/loa/loa.py
@@ -3,7 +3,8 @@
 
 # import frappe
 from frappe.model.document import Document
-
+import frappe
+from frappe.utils import today
 
 class LOA(Document):
 	# begin: auto-generated types
@@ -19,19 +20,21 @@ class LOA(Document):
 		allocated_driver_quota: DF.Int
 		allocated_vehicle_quota: DF.Int
 		amended_from: DF.Link | None
-		contract_number: DF.Data
+		client: DF.Link
+		contract_number: DF.Data | None
 		contract_year: DF.Int
 		document: DF.Attach | None
-		end_user: DF.Link
+		end_user: DF.Link | None
 		expiry_date: DF.Date
 		issue_date: DF.Date
 		issuing_authority: DF.Link
 		license_expiry_date: DF.Date | None
 		license_issue_date: DF.Date | None
-		loa_status: DF.Literal["", "Active", "Expired"]
+		loa_number: DF.Data
+		loa_status: DF.Literal["", "Active", "Expired", "Cancelled"]
 		locations: DF.Table[LOAlocationscdt]
 		mother_attachment: DF.Attach | None
-		ref_no: DF.Data
+		ref_no: DF.Data | None
 		remaining_driver_quota: DF.Int
 		remaining_vehicle_quota: DF.Int
 		total_cancelled_driver_cicpa: DF.Int
@@ -41,4 +44,62 @@ class LOA(Document):
 		total_driver_quota: DF.Int
 		total_vehicle_quota: DF.Int
 	# end: auto-generated types
-	pass
+	
+	def validate(self):
+		self.set_loa_status()
+
+	def on_cancel(self):
+		self.db_set("loa_status", "Cancelled")
+
+	def set_loa_status(self):
+
+		if self.expiry_date and self.expiry_date < today():
+			self.loa_status = "Expired"
+
+		else:
+			self.loa_status = "Active"
+
+
+def update_expired_loa_status():
+
+	expired_loas = frappe.get_all(
+		"LOA",
+		filters={
+			"expiry_date": ["<", today()],
+			"docstatus": ["!=", 2],
+			"loa_status": ["!=", "Expired"]
+		},
+		pluck="name"
+	)
+
+	for loa in expired_loas:
+
+		frappe.db.set_value(
+			"LOA",
+			loa,
+			{
+				"loa_status": "Expired"
+			},
+			update_modified=False
+		)
+
+	active_loas = frappe.get_all(
+		"LOA",
+		filters={
+			"expiry_date": [">=", today()],
+			"docstatus": ["!=", 2],
+			"loa_status": ["!=", "Active"]
+		},
+		pluck="name"
+	)
+
+	for loa in active_loas:
+
+		frappe.db.set_value(
+			"LOA",
+			loa,
+			{
+				"loa_status": "Active"
+			},
+			update_modified=False
+		)
